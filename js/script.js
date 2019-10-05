cb27904340976eceebe0c3920838f9db3d95d575
YuriiOne <yurgens87@gmail.com>
2019-10-06 00:05:37 +0300

    add scroll on top

--- js/script.js
+++ js/script.js
@@ -9,6 +9,8 @@
 showMyMenu();
 
 */
+
+
 document.addEventListener('DOMContentLoaded',()=>{
 	console.log('ContentLoaded')
 	let buttonMenu = document.querySelector('.navbar-toggle');
@@ -27,3 +29,25 @@ document.addEventListener('DOMContentLoaded',()=>{
 	}
 	closeOnClick()
 })
+$(document).ready(function(){
+  $('body').append('<a href="#" id="go-top" title="Back to top">Back to top</a>');
+});
+
+$(function() {
+ $.fn.scrollToTop = function() {
+  $(this).hide().removeAttr("href");
+  if ($(window).scrollTop() >= "250") $(this).fadeIn("slow")
+  var scrollDiv = $(this);
+  $(window).scroll(function() {
+   if ($(window).scrollTop() <= "250") $(scrollDiv).fadeOut("slow")
+   else $(scrollDiv).fadeIn("slow")
+  });
+  $(this).click(function() {
+   $("html, body").animate({scrollTop: 0}, "slow")
+  })
+ }
+});
+
+$(function() {
+ $("#go-top").scrollToTop();
+});
